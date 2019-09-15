3671ec25c4e566f3e36e8a7edbc7a04ce14f620b
Sam Lee <lee344@usc.edu>
2019-09-15 13:14:50 -0700

    changed port back

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 // const PORT = 80;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
