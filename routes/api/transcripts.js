c611c711d0647a4a5a33444f0a18b5f4de289611
Sam Lee <lee344@usc.edu>
2019-10-06 14:11:39 -0700

    calculates local viewership

--- routes/api/transcripts.js
+++ routes/api/transcripts.js
@@ -279,7 +279,18 @@ router.post('/receiver', async (req, res) => {
     }
     transcriptFields.viewership = 'n/a';
     //figure out local viewership data
-
+    if (
+      JSONRes.Message.Body.Page.BroadcastMetadata.ViewershipData
+        .LocalViewershipData.DMADemos
+    ) {
+      var obj =
+        JSONRes.Message.Body.Page.BroadcastMetadata.ViewershipData
+          .LocalViewershipData.DMADemos;
+      transcriptFields.viewership = 0;
+      Object.keys(obj).forEach(function(key) {
+        transcriptFields.viewership += obj[key];
+      });
+    }
     transcriptFields.totalViewership = 'n/a';
     console.log(transcriptFields);
     transcript = new Transcript(transcriptFields);
